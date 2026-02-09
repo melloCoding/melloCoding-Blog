abd564ba024f297cf0a038397f5316a264b7f660
Jeffrey Mello <mrjeffreymello@gmail.com>
2026-02-09 06:16:14 -0500

    update base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "dev.mellocoding.com",
+    baseUrl: "blog.mellocoding.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
