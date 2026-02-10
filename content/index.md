bdc8585cbb1fad67ad0c1bf0095a09ef7e2064c2
Jeffrey Mello <mrjeffreymello@gmail.com>
2026-02-09 21:21:49 -0500

    modified background futhrer

--- content/index.md
+++ content/index.md
@@ -1,5 +1,4 @@
 ---
 title: Blog Home
 ---
-# melloCoding Blog
 Where I will provide updates and documentation on my projects and other things I am currently working on like [[openHitbox/openHitboxindex |openHitbox]] 
--- quartz/components/background-blob.tsx
+++ quartz/components/background-blob.tsx
@@ -14,8 +14,8 @@ const BackgroundBlob = () => {
     position: "fixed",
     top: 0,
     left: 0,
-    width: "100%",
-    height: "100%",
+    size: "cover",
+    //height: "100%",
     zIndex: -1,
     filter: "blur(75px)",
   };
